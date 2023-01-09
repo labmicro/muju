3bb8a67319dc970403a9ae87789d768fc1b0113f
Esteban Volentini <evolentini@herrera.unt.edu.ar>
2023-01-09 09:58:03 -0300

    #1 Create initial structure of framework

---
+++ module/template/inc/template.h
@@ -0,0 +1,57 @@
+/************************************************************************************************
+Copyright (c) <year>, <copyright holders>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+SPDX-License-Identifier: MIT
+*************************************************************************************************/
+
+#ifndef TEMPLATE_H
+#define TEMPLATE_H
+
+/** \brief Brief description of the file
+ **
+ ** Full file description
+ **
+ ** \addtogroup name Module denomination
+ ** \brief Brief description of the module
+ ** @{ */
+
+/* === Headers files inclusions ================================================================ */
+
+/* === Cabecera C++ ============================================================================ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* === Public macros definitions =============================================================== */
+
+/* === Public data type declarations =========================================================== */
+
+/* === Public variable declarations ============================================================ */
+
+/* === Public function declarations ============================================================ */
+
+/* === End of documentation ==================================================================== */
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} End of module definition for doxygen */
+
+#endif /* TEMPLATE_H */
