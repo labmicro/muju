407f4be985caa5a78a4f1bba2bde9a440834c5fa
Esteban Volentini <evolentini@herrera.unt.edu.ar>
2023-02-21 17:14:06 -0300

    #3 Improvement support for EDU-CIAA-NXP board

--- board/edu-ciaa-nxp/inc/board.h
+++ board/edu-ciaa-nxp/inc/board.h
@@ -30,7 +30,12 @@ SPDX-License-Identifier: MIT
 
 /* === Headers files inclusions ================================================================ */
 
+#if defined(USE_HAL)
+#include "hal.h"
+#endif
+#if defined(USE_DRIVERS)
 #include "chip.h"
+#endif
 
 /* === Cabecera C++ ============================================================================ */
 
@@ -39,6 +44,81 @@ extern "C" {
 #endif
 
 /* === Public macros definitions =============================================================== */
+
+#if defined(USE_HAL)
+#define LED_R HAL_GPIO5_0
+#define LED_G HAL_GPIO5_1
+#define LED_B HAL_GPIO5_2
+
+#define LED_1 HAL_GPIO0_14
+#define LED_2 HAL_GPIO1_11
+#define LED_3 HAL_GPIO1_12
+
+#define TEC_1 HAL_GPIO0_4
+#define TEC_2 HAL_GPIO0_8
+#define TEC_3 HAL_GPIO0_9
+#define TEC_4 HAL_GPIO1_9
+#elif defined(USE_DRIVERS)
+#define LED_R_PORT 2
+#define LED_R_PIN  0
+#define LED_R_FUNC SCU_MODE_FUNC4
+#define LED_R_GPIO 5
+#define LED_R_BIT  0
+
+#define LED_G_PORT 2
+#define LED_G_PIN  1
+#define LED_G_FUNC SCU_MODE_FUNC4
+#define LED_G_GPIO 5
+#define LED_G_BIT  1
+
+#define LED_B_PORT 2
+#define LED_B_PIN  2
+#define LED_B_FUNC SCU_MODE_FUNC4
+#define LED_B_GPIO 5
+#define LED_B_BIT  2
+
+#define LED_1_PORT 2
+#define LED_1_PIN  10
+#define LED_1_FUNC SCU_MODE_FUNC0
+#define LED_1_GPIO 0
+#define LED_1_BIT  14
+
+#define LED_2_PORT 2
+#define LED_2_PIN  11
+#define LED_2_FUNC SCU_MODE_FUNC0
+#define LED_2_GPIO 1
+#define LED_2_BIT  11
+
+#define LED_3_PORT 2
+#define LED_3_PIN  12
+#define LED_3_FUNC SCU_MODE_FUNC0
+#define LED_3_GPIO 1
+#define LED_3_BIT  12
+
+#define TEC_1_PORT 1
+#define TEC_1_PIN  0
+#define TEC_1_FUNC SCU_MODE_FUNC0
+#define TEC_1_GPIO 0
+#define TEC_1_BIT  4
+
+#define TEC_2_PORT 1
+#define TEC_2_PIN  1
+#define TEC_2_FUNC SCU_MODE_FUNC0
+#define TEC_2_GPIO 0
+#define TEC_2_BIT  8
+
+#define TEC_3_PORT 1
+#define TEC_3_PIN  2
+#define TEC_3_FUNC SCU_MODE_FUNC0
+#define TEC_3_GPIO 0
+#define TEC_3_BIT  9
+
+#define TEC_4_PORT 1
+#define TEC_4_PIN  6
+#define TEC_4_FUNC SCU_MODE_FUNC0
+#define TEC_4_GPIO 1
+#define TEC_4_BIT  9
+#endif
 
 /* === Public data type declarations =========================================================== */
 
